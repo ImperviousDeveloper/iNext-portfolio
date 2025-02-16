b1f8f5bb984e1ab463545955886f975192f5d651
Rajan Kumar Sharma <r7870062870@gmail.com>
2025-02-17 03:59:52 +0530

    add experience

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Hero from "@/components/Hero";
 import Grid from "@/components/Grid";
 import RecentProjects from "@/components/RecentProjects";
 import Clients from "@/components/Clients";
+import Experience from "@/components/Experience";
 
 export default function Home() {
   return (
@@ -14,7 +15,7 @@ export default function Home() {
           <Grid/>
           <RecentProjects/>
           <Clients/>
-          {/*<Experience/>*/}
+          <Experience/>
           {/*<Approach/>*/}
           {/*<Footer/>*/}
         </div>
